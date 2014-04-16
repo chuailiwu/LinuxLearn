d1bb388392a251b8c1b877368212213b8564ae66
joseph1002 <joseph1002.lee@gmail.com>
2014-04-16 15:17:37 +0800

    inital commit

--- include/public.h
+++ include/public.h
@@ -17,6 +17,10 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
 
 #define	MAXLINE	4096			/* max line length */
 #define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
@@ -30,6 +34,17 @@ void err_quit(const char *fmt, ...);
 void pr_mask(const char *str);
 int already_running(const char* lock_file);
 
+#define	SA	struct sockaddr
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
+int Accept(int, SA *, socklen_t *);
+void Bind(int, const SA *, socklen_t);
+void Connect(int, const SA *, socklen_t);
+void Listen(int fd, int backlog);
+
+ssize_t readline(int fd, void *vptr, size_t maxlen);
+ssize_t readlinebuf(void **vptrptr);
+ssize_t Readline(int fd, void *ptr, size_t maxlen);
 
 #endif
 
